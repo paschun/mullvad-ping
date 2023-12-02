4b30f2feef3fee5511b5611af3ef35199e86bdd5
paschun <152430265+paschun@users.noreply.github.com>
2023-12-01 21:58:52 -0800

    Update to be async
    Remove windows support

--- script.ts
+++ script.ts
@@ -1,4 +1,4 @@
-import { parse } from "https://deno.land/std@0.201.0/flags/mod.ts";
+import { parseArgs } from "https://deno.land/std@0.208.0/cli/parse_args.ts";
 
 type ServerDataJSON = {
   hostname: string;
@@ -16,8 +16,21 @@ type ServerDataJSON = {
   type: string;
 };
 
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+type ServerPingData = {
+  hostname: string;
+  city: string;
+  country: string;
+  type: string;
+  ip: string;
+  avg: number;
+  network_port_speed: number;
+}
+
+let lastSleep = 0;
+function sleep() {
+  lastSleep += 10
+  const thisSleep = lastSleep
+  return new Promise((resolve) => setTimeout(resolve, thisSleep));
 }
 
 function checkRunMode(stboot: boolean, runMode: string) {
@@ -34,8 +47,8 @@ function checkRunMode(stboot: boolean, runMode: string) {
 const serverTypes = ["openvpn", "bridge", "wireguard", "all"];
 const runTypes = ["all", "ram", "disk"];
 
-const args = parse(Deno.args);
-if (args.help == true) {
+const args = parseArgs(Deno.args);
+if (args.help || args.h) {
   console.log(`Usage: script [OPTION]
     --country <code>      the country you want to query (eg. us, gb, de)
     --list-countries      lists the available countries
@@ -61,8 +74,8 @@ if (args.help == true) {
   Deno.exit(0);
 }
 
-const country = args.country?.toLowerCase();
-const serverType = args.type?.toLowerCase() ?? "all";
+const country = args.country?.toLowerCase() ?? "ch";
+const serverType = args.type?.toLowerCase() ?? "wireguard";
 if (!serverTypes.includes(serverType)) {
   throw new Error(`Invalid type, allowed types are: ${serverTypes.join(", ")}`);
 }
@@ -71,7 +84,7 @@ const interval = parseFloat(args.interval ?? 0.2) || 0.2;
 if (interval < 0.2) {
   throw new Error("Minimum interval value is 0.2");
 }
-const count = parseInt(args.count ?? 5) || 5;
+const count = parseInt(args.count ?? 3) || 3;
 const topN = parseInt(args.top ?? 5) || 5;
 const portSpeed = parseInt(args["port-speed"] ?? 0) || 0;
 
@@ -101,6 +114,33 @@ const response = await fetch(
 );
 const json: Array<ServerDataJSON> = await response.json();
 
+function parsePing(output: string, server: ServerDataJSON) {
+  // [all, min, avg, max, mdev]
+  const regex =
+    /(?<min>\d+(?:.\d+)?)\/(?<avg>\d+(?:.\d+)?)\/(?<max>\d+(?:.\d+)?)\/(?<mdev>\d+(?:.\d+)?)/;
+
+  const values = output.match(regex);
+  if (values) {
+    console.log(
+      `Pinged ${server.hostname}.mullvad.net, min/avg/max/mdev ${values[0]
+      }`,
+    );
+
+    const result: ServerPingData = {
+      hostname: server.hostname,
+      city: server.city_name,
+      country: server.country_name,
+      type: server.type,
+      ip: server.ipv4_addr_in,
+      avg: parseFloat(values[2]) || 0,
+      network_port_speed: server.network_port_speed,
+    };
+    return result
+  }
+  console.error(`no output match for ${server.hostname} - ${output}`)
+  return null
+}
+
 if (args["list-countries"]) {
   const countries = new Set();
   json.forEach((e) => {
@@ -110,7 +150,7 @@ if (args["list-countries"]) {
     console.log(e);
   });
 } else {
-  const results = [];
+  const pings = [];
 
   for (const server of json) {
     if (
@@ -120,18 +160,13 @@ if (args["list-countries"]) {
       (provider == null || provider == server.provider) &&
       (owned == null || owned == server.owned)
     ) {
-      let args = [];
-      if (Deno.build.os == "windows") {
-        args = ["-n", count.toString(), server.ipv4_addr_in];
-      } else {
-        args = [
-          "-c",
-          count.toString(),
-          "-i",
-          interval.toString(),
-          server.ipv4_addr_in,
-        ];
-      }
+      const args = [
+        "-c",
+        count.toString(),
+        "-i",
+        interval.toString(),
+        server.ipv4_addr_in,
+      ];
 
       const p = new Deno.Command(
         "ping",
@@ -141,59 +176,24 @@ if (args["list-countries"]) {
         },
       );
 
-      const output = new TextDecoder().decode((await p.output()).stdout);
-
-      if (Deno.build.os == "windows") {
-        // [all, min, avg, max, mdev]
-        const regex = /Average = (\d*)ms/;
-        const avg = output.match(regex);
-        if (avg) {
-          console.log(`Pinged ${server.hostname}.mullvad.net, avg ${avg[1]}ms`);
-
-          results.push({
-            hostname: server.hostname,
-            city: server.city_name,
-            country: server.country_name,
-            type: server.type,
-            ip: server.ipv4_addr_in,
-            avg: parseFloat(avg[1]) || 0,
-            network_port_speed: server.network_port_speed,
-          });
-        }
-
-        await sleep(200);
-      } else {
-        // [all, min, avg, max, mdev]
-        const regex =
-          /(?<min>\d+(?:.\d+)?)\/(?<avg>\d+(?:.\d+)?)\/(?<max>\d+(?:.\d+)?)\/(?<mdev>\d+(?:.\d+)?)/;
-
-        const values = output.match(regex);
-        if (values) {
-          console.log(
-            `Pinged ${server.hostname}.mullvad.net, min/avg/max/mdev ${
-              values[0]
-            }`,
-          );
-
-          results.push({
-            hostname: server.hostname,
-            city: server.city_name,
-            country: server.country_name,
-            type: server.type,
-            ip: server.ipv4_addr_in,
-            avg: parseFloat(values[2]) || 0,
-            network_port_speed: server.network_port_speed,
-          });
-        }
-
-        await sleep(interval * 1000);
-      }
+      console.log('sending ping to ', server.ipv4_addr_in)
+      pings.push(sleep().then(_ => p.output()).then((cmdout: Deno.CommandOutput) => {
+        const decoder = new TextDecoder()
+        const stderr = decoder.decode(cmdout.stderr)
+        if (stderr) throw new Error(stderr)
+        const stdout = decoder.decode(cmdout.stdout)
+        return parsePing(stdout, server)
+      }))
     }
   }
+  
+  const results = (await Promise.all(pings)).filter((r): r is ServerPingData => r !== null)
 
   results.sort((a, b) => {
     return a.avg - b.avg;
-  });
+  })
+
+  // console.log(results)
 
   const top = topN == 0 ? results : results.slice(0, topN);
 
@@ -207,7 +207,8 @@ if (args["list-countries"]) {
         }ms) ${e.network_port_speed} Gigabit ${e.type} ${e.city}, ${e.country}`,
       );
     }
-    console.table();
+    console.log('')
+    console.log(top[0].hostname)
   } else {
     console.error("No servers found");
   }
